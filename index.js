a0daddaa2d799f4563843ba91a6028b9e6f463b9
Michael Murphy <michaelhmurphy99@gmail.com>
2022-07-05 15:19:54 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string)  {
+    console.log("HELLO");
+    return string.toUpperCase();
+  }
+  function logWhisper(string)  {
+    console.log("hello");
+    return string.toLowerCase();
+  }
+  function sayHiToHeadphonedRoomate(string)  {
+    console.log("I can't hear you!")
+    
+  }
